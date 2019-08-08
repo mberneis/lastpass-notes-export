d3a1d1d385bb1786a91f535a898bae477d88a713
Michael Berneis <mberneis@gmail.com>
2019-08-08 10:16:24 -0700

    small refactor and updated readme

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 Utility to export Lastpass Secure Notes
 
 ## Usage (tested with Chrome)
-> Goto LastPass Menu:
-> Account Options -> Advanced -> Print -> Secure Notes
+Goto LastPass Menu:
+`Account Options -> Advanced -> Print -> Secure Notes`
 ### Notes appear in Browser Window
-> In Chrome: 
-> File -> Save Page As -> (Format: WebPage Complete)  in code directory (will be excluded by `.gitignore`)
-> Run `php export.php` -> will generate `Print.csv`
+In Chrome: 
+`File -> Save Page As -> (Format: WebPage Complete)`  in code directory (will be excluded by `.gitignore`)
+
+Run `php export.php` -> will reae `Print.htm` and generate `Print.csv`
+
+Use `Print.csv` with the import format `Bitwarden (csv)` to import the secure notes into Bitwarden
 
 This Project uses the `simple_html_dom` library from  Website: http://sourceforge.net/projects/simplehtmldom/
 
--- export.php
+++ export.php
@@ -1,18 +1,25 @@
 <?php
+/*
+ This app will convert the HTML from LastPasss Print Secure Notes Utility into the Bitwarden Format for easy import
+
+ @author: Michael Berneis
+ @data 8/8/2019
+
+ please read Readme.md for instructions
+*/
 
 include('simple_html_dom.php');
  
 $html = file_get_html("Print.htm");
-$data = $html->find('div#output')[0];
-$fp = fopen("Print.csv", "w");
+$data = $html->find('div#output', 0);
+$fp   = fopen("Print.csv", "w");
 fwrite($fp, "folder,favorite,type,name,notes,fields,login_uri,login_username,login_password,login_totp\n");
 foreach ($data->find("table.x-grid3-row-table") as $note) {
     $sections = $note->find("td.x-grid3-col");
-    $name = trim($sections[0]->find("p")[0]->innertext, '" ');
-    $folder = trim($sections[1]->find("p")[0]->innertext, '" ');
-    $content = trim($sections[2]->find("p")[0]->innertext, '" ');
-    $content = str_replace('<br>', "\n", $content);
-    $line = [$folder,'','note',$name,$content,'','','','',''];
-    fputcsv($fp, $line);
+    $name     = trim($sections[0]->find("p", 0)->innertext, '" ');
+    $folder   = trim($sections[1]->find("p", 0)->innertext, '" ');
+    $content  = trim($sections[2]->find("p", 0)->innertext, '" ');
+    $content  = str_replace('<br>', "\n", $content);
+    fputcsv($fp, [$folder,'','note',$name,$content,'','','','','']);
 }
 fclose($fp);
